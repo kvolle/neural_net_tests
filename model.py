91b27914b5ee251dbe7a1784d9e4809dc49718ba
Kyle Volle <kvolle3@gatech.edu>
2018-08-17 09:02:22 -0500

    Changes to make model fit the tensorflow pardigm better

--- model.py
+++ model.py
@@ -11,19 +11,27 @@ class network:
         self.y2 = tf.placeholder(tf.float32, [None])
         self.loss = self.custom_loss()
         previous = 784
-        i = 0
+
         self.layers = []
-        input_layer = tf.placeholder(tf.float32, shape=[None, 784])
+
+         with tf.variable_scope("siamese") as scope:
+            self.o1 = self.network(self.x1)
+            scope.reuse_variables()
+            self.o2 = self.network(self.x2)
+
+    def sub_network_generation(self, input, sizes):
+        i = 0
         for x in sizes:
-            self.layers.append(self.layer_generation(input_layer, x, "layer"+str(i)))
+            self.layers.append(self.layer_generation(input, x, "layer"+str(i)))
             i=i+1
+            input=self.layers[-1]
 
     def layer_generation(self, input, layer_size, name):
         input_len = input.get_shape()[1]
         seed = tf.truncated_normal_initializer(stddev=0.01)
         w = tf.get_variable(name+'_W', dtype=tf.float32, shape=[input_len, layer_size], initializer=seed)
         b = tf.get_variable(name+'_b', dtype=tf.float32, initializer=tf.constant(0.01, shape=[layer_size], dtype=tf.float32) )
-        fc = tf.nn.bias_add(tf.matmul(input, w), b)
-        return fc
+        out = tf.nn.relu(tf.nn.bias_add(tf.matmul(input, w), b), name=name+'_out')
+        return out
 
     def custom_loss(self):
