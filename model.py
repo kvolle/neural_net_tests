ac6fa0af7fe8d721324322f8a31ccdafbd32eb13
Kyle Volle <kvolle3@gatech.edu>
2018-08-21 07:11:57 -0500

    My version of the loss function is commented out, changed function names, class names and renamed input so as not to be overshadow the builtin

--- model.py
+++ model.py
@@ -1,30 +1,29 @@
 import tensorflow as tf
 
-class network:
+class siamese:
 
     # Create model
     def __init__(self, sizes):
         self.x1 = tf.placeholder(tf.float32, [None, 784])
         self.x2 = tf.placeholder(tf.float32, [None, 784])
-        # Create loss
-        self.y1 = tf.placeholder(tf.float32, [None])
-        self.y2 = tf.placeholder(tf.float32, [None])
-        self.loss = self.custom_loss()
-        previous = 784
-
         self.layers = []
-
-         with tf.variable_scope("siamese") as scope:
-            self.o1 = self.network(self.x1)
+        with tf.variable_scope("siamese") as scope:
+            self.o1 = self.network(self.x1,sizes)
             scope.reuse_variables()
-            self.o2 = self.network(self.x2)
+            self.o2 = self.network(self.x2,sizes)
 
-    def sub_network_generation(self, input, sizes):
+        # Create loss
+        self.y_ = tf.placeholder(tf.float32, [None])
+        self.loss = self.custom_loss()
+
+    def network(self, input_layer, sizes):
+
         i = 0
         for x in sizes:
-            self.layers.append(self.layer_generation(input, x, "layer"+str(i)))
+            self.layers.append(self.layer_generation(input_layer, x, "layer"+str(i)))
             i=i+1
-            input=self.layers[-1]
+            input_layer=self.layers[-1]
+        return self.layers[-1]
 
     def layer_generation(self, input, layer_size, name):
         input_len = input.get_shape()[1]
@@ -35,3 +34,32 @@ class network:
         return out
 
     def custom_loss(self):
+        margin = 5.0
+        labels_t = self.y_
+        labels_f = tf.subtract(1.0, self.y_, name="1-yi")          # labels_ = !labels;
+        eucd2 = tf.pow(tf.subtract(self.o1, self.o2), 2)
+        eucd2 = tf.reduce_sum(eucd2, 1)
+        eucd = tf.sqrt(eucd2+1e-6, name="eucd")
+        C = tf.constant(margin, name="C")
+        pos = tf.multiply(labels_t, eucd2, name="yi_x_eucd2")
+        neg = tf.multiply(labels_f, tf.pow(tf.maximum(tf.subtract(C, eucd), 0), 2), name="Nyi_x_C-eucd_xx_2")
+        losses = tf.add(pos, neg, name="losses")
+        loss = tf.reduce_mean(losses, name="loss")
+        return loss
+
+"""
+    def custom_loss(self):
+        margin=5.0
+        #different_class_examples = tf.subtract(tf.constant(1.0, dtype=tf.float32), self.y_, name="diff_class")
+        different_class_examples = tf.subtract(1.0, self.y_, name="diff_class")
+        labels_f = tf.subtract(1.0, self.y_, name="1-yi")
+        distance = tf.pow(tf.subtract(self.o1, self.o2), 2)
+        distance = tf.reduce_sum(distance, 1)
+        distance = tf.sqrt(distance+1e-6, name="Distance")
+        same_class_losses = tf.multiply(self.y_, distance)
+        margin_tensor = tf.constant(margin,dtype=tf.float32, name="Margin")
+        diff_class_losses = tf.multiply(labels_f, tf.pow(tf.maximum(0.0, tf.subtract(margin_tensor, distance)), 2.))
+        losses = tf.add(same_class_losses, diff_class_losses)
+        loss = tf.reduce_mean(losses, name="loss")#losses, name="loss")
+        return loss
+"""
