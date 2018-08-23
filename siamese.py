8c6526de5037c3b54e3530e99904ef5d046f5487
Kyle Volle <kvolle3@gatech.edu>
2018-08-23 16:09:22 -0500

    Revert "mend"
    This reverts commit e735bf8e3ae75ccc5f9a9694504ddf97bf7e4e59.

--- siamese.py
+++ siamese.py
@@ -13,29 +13,23 @@ import os
 #tf.logging.set_verbosity(tf.logging.ERROR)
 
 #import helpers
-import modelMine
-from siamese_tf_mnist import visualize
+import model
 
 # prepare data and tf.session
 mnist = input_data.read_data_sets('MNIST_data', one_hot=False)
 sess = tf.InteractiveSession()
 
 # setup siamese network
-network = modelMine.siamese([1024, 512, 2])
+network = model.siamese([1024, 1024, 2])
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(network.loss)
 #saver = tf.train.Saver()
 tf.initialize_all_variables().run()
 
-testing = network.o1.eval({network.x1: mnist.test.images})
-np.savetxt("labels_prior.csv", mnist.test.labels, delimiter=",")
-np.savetxt("output_prior.csv", testing, delimiter=",")
-writer = tf.summary.FileWriter("log/Kyle/",sess.graph)
-
-for step in range(2000):
+for step in range(2000):#(50000):
     batch_x1, batch_y1 = mnist.train.next_batch(128)
     batch_x2, batch_y2 = mnist.train.next_batch(128)
-    batch_y = (batch_y1 == batch_y2)
-
+    batch_y = (batch_y1 == batch_y2).astype('float')
+    
     _, loss_v = sess.run([train_step, network.loss], feed_dict={
                         network.x1: batch_x1,
                         network.x2: batch_x2,
@@ -47,17 +41,3 @@ for step in range(2000):
 
     if step % 10 == 0:
         print ('step %d: loss %.3f' % (step, loss_v))
-
-    if (step + 1) % 1000 == 0:
-        #saver.save(sess, './model')
-        embed = network.o1.eval({network.x1: mnist.test.images})
-        embed.tofile('embed.txt')
-
-np.savetxt("labels.csv", mnist.test.labels, delimiter=",")
-np.savetxt("output.csv", embed, delimiter=",")
-writer.close()
-
-# visualize result
-#x_test = mnist.test.images.reshape([-1, 28, 28])
-#y_test = mnist.test.labels
-#visualize.visualize(embed, x_test, y_test)
