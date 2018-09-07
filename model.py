0cdeb2428478472b9291d61afd263868b05ebdcd
Kyle Volle <kvolle3@gatech.edu>
2018-09-06 22:42:20 -0500

    Diverges, but can create the model and attempt training

--- model.py
+++ model.py
@@ -4,8 +4,12 @@ class siamese:
 
     # Create model
     def __init__(self, sizes):
-        self.x1 = tf.placeholder(tf.float32, [None, 784])
-        self.x2 = tf.placeholder(tf.float32, [None, 784])
+        self.keep_prob = 0.5#tf.placeholder(tf.float32, name='dropout_prob')
+        self.num_labels = 10#tf.placeholder(tf.float32, name="num_layers")
+        #self.x1 = tf.placeholder(tf.float32, [None, 784])
+        #self.x2 = tf.placeholder(tf.float32, [None, 784])
+        self.x1 = tf.placeholder(dtype=tf.float32, shape=[None, 28, 28, 1])
+        self.x2 = tf.placeholder(dtype=tf.float32, shape=[None, 28, 28, 1])
         self.layers = []
         with tf.variable_scope("siamese") as scope:
             self.o1 = self.network(self.x1,sizes)
@@ -17,14 +21,34 @@ class siamese:
         self.loss = self.custom_loss()
 
     def network(self, input_layer, sizes):
-        i = 0
+        #i = 0
         input_layer_local = input_layer
+        out_1 = self.conv_layer(input_layer_local, [5,5,1,32],'layer1')
+        out_2 = self.conv_layer(out_1, [5, 5, 32, 64],'layer2')
+        with tf.variable_scope('local1') as scope:
+            reshape = tf.reshape(out_2, [-1, 7 * 7 * 64])
+            #W_fc1 = self._create_weights([7 * 7 * 64, 1024])
+            #b_fc1 = self._create_bias([1024])
+
+            W_fc1 = tf.Variable(tf.truncated_normal(shape=[7 * 7 * 64, 1024], stddev=0.1, dtype=tf.float32))
+            b_fc1 = tf.Variable(tf.constant(1., shape=[1024], dtype=tf.float32))
+            local1 = tf.nn.relu(tf.matmul(reshape, W_fc1) + b_fc1, name=scope.name)
+            #self._activation_summary(local1)
+
+        with tf.variable_scope('local2_linear') as scope:
+            W_fc2 = tf.Variable(tf.truncated_normal(shape=[1024, self.num_labels], stddev=0.1, dtype=tf.float32))
+            b_fc2 = tf.Variable(tf.constant(1., shape=[self.num_labels], dtype=tf.float32))
+            local1_drop = tf.nn.dropout(local1, self.keep_prob)
+            local2 = tf.nn.bias_add(tf.matmul(local1_drop, W_fc2), b_fc2, name=scope.name)
+            #self._activation_summary(local2)
+        return local2
+    """
         for x in sizes:
             self.layers.append(self.layer_generation(input_layer_local, x, "layer" + str(i)))
             i = i + 1
             input_layer_local = tf.nn.relu(self.layers[-1], name='out_'+str(i))
         return self.layers[-1]
-
+    """
     def layer_generation(self, input, layer_size, name):
         input_len = input.get_shape()[1]
         seed = tf.truncated_normal_initializer(stddev=0.01)
@@ -35,9 +59,9 @@ class siamese:
         return out
 
     def conv2d(self, input_layer, W):
-        return tf.nn.conv2d(input= input_layer,
+        return tf.nn.conv2d(input=input_layer,
                             filter=W,
-                            strides=[1,1,1,1],
+                            strides=[1, 1, 1, 1],
                             padding='SAME')
 
     def create_max_pool_layer(self, input):
@@ -50,15 +74,16 @@ class siamese:
         tf.summary.histogram(tensor_name + '/activations', x)
         tf.summary.scalar(tensor_name + '/sparsity', tf.nn.zero_fraction(x))
 
-    def conv_layer(self, weights, name):
+    def conv_layer(self, input_layer, weights, name):
         with tf.variable_scope(name) as scope:
             kernel = tf.Variable(tf.truncated_normal(shape=weights, stddev=0.1, dtype=tf.float32))
-            conv = self.conv2d(images, kernel)
+            conv = self.conv2d(input_layer, kernel)
             bias = tf.Variable(tf.constant(1., shape=[weights[-1]], dtype=tf.float32))
             preactivation = tf.nn.bias_add(conv, bias)
             conv_relu = tf.nn.relu(preactivation, name=scope.name)
             self.activation_summary(conv_relu)
             h_pool = self.create_max_pool_layer(conv_relu)
+        return h_pool
 
     def custom_loss(self):
         margin=5.0
