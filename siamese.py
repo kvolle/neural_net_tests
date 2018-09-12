cb18f07d3e7d1c7cfc24a4a4598532c2901c38a9
Kyle Volle <kvolle3@gatech.edu>
2018-09-12 14:32:23 -0500

    Trained for longer and commented out the rotation

--- siamese.py
+++ siamese.py
@@ -42,13 +42,15 @@ np.savetxt("labels_prior.csv", mnist.test.labels, delimiter=",")
 np.savetxt("output_prior.csv", testing, delimiter=",")
 """
 writer = tf.summary.FileWriter("log/Kyle/",sess.graph)
-for step in range(2000):
+N = 5000
+for step in range(N):
     long_x1, batch_y1 = mnist.train.next_batch(128)
     long_x2, batch_y2 = mnist.train.next_batch(128)
     batch_y = (batch_y1 == batch_y2)
     batch_x1 = long_x1.reshape(len(long_x1), image_size, image_size, 1)
     batch_x2 = long_x2.reshape(len(long_x1), image_size, image_size, 1)
-    batch_x2 = noise(batch_x2)
+    #if (step % 10) != 0:
+    #    batch_x2 = noise(batch_x2)
     _, loss_v = sess.run([train_step, network.loss], feed_dict={
                         network.x1: batch_x1,
                         network.x2: batch_x2,
@@ -57,13 +59,14 @@ for step in range(2000):
     if np.isnan(loss_v):
         print('Model diverged with loss = NaN')
         quit()
-
-    if step % 600 == 0:
-        train_step = tf.train.GradientDescentOptimizer(0.0001*pow(2,step/600)).minimize(network.loss)
+    if step == 10:
+        train_step = tf.train.GradientDescentOptimizer(0.001).minimize(network.loss)
+#    if step % 600 == 0:
+#        train_step = tf.train.GradientDescentOptimizer(0.0001*pow(2,step/600)).minimize(network.loss)
     if step % 10 == 0:
         print ('step %d: loss %.3f' % (step, loss_v))
 
-    if (step + 1) % 2000 == 0:
+    if (step + 1) % N == 0:
         #saver.save(sess, './model')
         image_vector = mnist.test.images.reshape(len(mnist.test.images), image_size, image_size, 1)
         image_vector = image_vector[:1000,:,:,:]
